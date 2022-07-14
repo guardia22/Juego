f2bc69fbab04fcc1fa0e4867227be5f239a7288b
guardia22 <guardia.rp1@gmail.com>
2022-07-14 18:38:20 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { JuegoComponent } from './pages/juego/juego.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{path:'juego', component:JuegoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JuegoComponent } from './pages/juego/juego.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JuegoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
